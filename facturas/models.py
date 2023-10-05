3e63a33a57ba3c42bce6a25ff193b2abf4bec3b8
skarkloff <skarkloff.ls99@gmail.com>
2023-10-05 18:37:24 -0300

    cambio en mod. factura

--- facturas/models.py
+++ facturas/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Factura(models.Model):
+
     montoAbonado= models.DecimalField(decimal_places=2)
     fechaPago= models.DateField()
-    correo= models.CharField(max_length=90)
     FORMAS= ['efectivo','credito','cheque','transferencia']
-    formaPago= models.CharField(max_length=15,choices=TIPOS,default='casa')
-    habitual= models.BooleanField(default=False)
-    gubernamental=models.BooleanField(default=False)
+    formaPago= models.CharField(max_length=15,choices=FORMAS,default='efectivo')
 
-    def __str__(self):
-        return self.apellido_y_nombre
+
+
+
+

f658827164b4b68c8ef869c3e8f28002a687b7d6
skarkloff <skarkloff.ls99@gmail.com>
2023-10-06 16:25:49 -0300

    cambio en mod. v4 ahora si

--- facturas/models.py
+++ facturas/models.py
@@ -6,7 +6,7 @@ class Factura(models.Model):
 
     montoAbonado= models.DecimalField(decimal_places=2,max_digits=10)
     fechaPago= models.DateField()
-    FORMAS= [('efectivo','efectivo'),('credito','efectivo'),('cheque','efectivo'),('transferencia','efectivo')]
+    FORMAS= [('efectivo','efectivo'),('credito','credito'),('cheque','cheque'),('transferencia','transferencia')]
     formaPago= models.CharField(max_length=15,choices=FORMAS,default='efectivo')
 
 

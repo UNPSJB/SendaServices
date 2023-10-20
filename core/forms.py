33156e47b4e85bf04efc32412b5c05237c893c6c
AlejooGz <ale.maxigonzalez@gmail.com>
2023-10-20 00:10:11 -0300

    inmuebles, detalles del formulario

--- core/forms.py
+++ core/forms.py
@@ -12,66 +12,6 @@ class ClienteForm(ModelForm):
         #Label se refiere la descripcion que esta al lado del formulario.
         
 
-class ProductoForm(ModelForm):
-
-    class Meta:
-        model = Producto
-        fields = '__all__'
-        #Label se refiere la descripcion que esta al lado del formulario.
-        labels = { 
-            'codigo': 'Código',
-            'descripcion': 'Descripción',
-            'stock': 'Stock',
-            'precioUnitario': 'Precio Unitario',
-           
-        }
-        #Referencia a los estilos con los que se renderizan los campos
-        widgets = {
-            'codigo': forms.TextInput(
-                #Permite estilizar los formularios
-                attrs = {
-                    'class': 'form-control',
-                    'placeholder':'Ingrese el codigo del producto',
-                }
-            ),
-            'descripcion': forms.TextInput(
-                attrs = {
-                    'class': 'form-control',
-                    'placeholder':'Ingrese descripcion del producto',
-                }
-            ),
-            'stock': forms.NumberInput(
-                attrs = {
-                    'min': 0,
-                    'class': 'form-control',
-                    'placeholder':'Ingrese el stock del producto',
-
-                }
-            ),
-            'precioUnitario': forms.NumberInput(
-                attrs = {
-                    'min': 0,                    
-                    'class': 'form-control',
-                    'placeholder':'Ingrese el precio del producto',
-                }
-            ),
-        }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_id = 'id-productoForm'
-        self.helper.form_method = 'post'
-
-        self.helper.add_input(Submit('submit', 'Guardar'))
-
-
-class ProductoUpdateForm(ProductoForm):
-
-    class Meta(ProductoForm.Meta):
-        exclude = ["stock", "codigo"]
-
-
 class InmuebleForm(ModelForm):
 
     class Meta:
@@ -136,4 +76,64 @@ class InmuebleForm(ModelForm):
 class InmuebleUpdateForm(InmuebleForm):
 
     class Meta(InmuebleForm.Meta):
-        exclude = ["domicilio", "cliente.cuil_cuit"]
+        exclude = ["domicilio", "cliente"]
+
+
+class ProductoForm(ModelForm):
+
+    class Meta:
+        model = Producto
+        fields = '__all__'
+        #Label se refiere la descripcion que esta al lado del formulario.
+        labels = { 
+            'codigo': 'Código',
+            'descripcion': 'Descripción',
+            'stock': 'Stock',
+            'precioUnitario': 'Precio Unitario',
+           
+        }
+        #Referencia a los estilos con los que se renderizan los campos
+        widgets = {
+            'codigo': forms.TextInput(
+                #Permite estilizar los formularios
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder':'Ingrese el codigo del producto',
+                }
+            ),
+            'descripcion': forms.TextInput(
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder':'Ingrese descripcion del producto',
+                }
+            ),
+            'stock': forms.NumberInput(
+                attrs = {
+                    'min': 0,
+                    'class': 'form-control',
+                    'placeholder':'Ingrese el stock del producto',
+
+                }
+            ),
+            'precioUnitario': forms.NumberInput(
+                attrs = {
+                    'min': 0,                    
+                    'class': 'form-control',
+                    'placeholder':'Ingrese el precio del producto',
+                }
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-productoForm'
+        self.helper.form_method = 'post'
+
+        self.helper.add_input(Submit('submit', 'Guardar'))
+
+
+class ProductoUpdateForm(ProductoForm):
+
+    class Meta(ProductoForm.Meta):
+        exclude = ["stock", "codigo"]

3db3fa7277120e11c8af24b7744ff8603062c9cc
skarkloff <skarkloff.ls99@gmail.com>
2023-11-16 18:11:07 -0300

    turnos agregar asistencia

--- turnos/admin.py
+++ turnos/admin.py
@@ -2,12 +2,35 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+from django.utils.safestring import mark_safe
+from django.urls import reverse
 
+class LinkAgregarAsistencia(object):
+    def asistencias(self, instance):
+        url = reverse('admin:%s_%s_change' % (
+            instance._meta.app_label,  instance._meta.model_name),  args=[instance.pk] )
+        if instance.pk:
+            return mark_safe(u'<a href="{u}">Agregar asistencias</a>'.format(u=url))
+        else:
+            return ''
 
+class AsistenciaInline(admin.TabularInline):
+    model = Asistencia
 
-
-class PeriodoInline(admin.TabularInline):
+class PeriodoAdmin(admin.ModelAdmin):
     model = Periodo
+    inlines = [
+        AsistenciaInline
+    ]
+
+
+class PeriodoInline(LinkAgregarAsistencia, admin.TabularInline):
+    model = Periodo
+    readonly_fields = ('asistencias', )
+    inlines = [
+        AsistenciaInline
+    ]
+        
 
 class HorarioAdmin(admin.ModelAdmin):
     inlines = [
@@ -15,5 +38,7 @@ class HorarioAdmin(admin.ModelAdmin):
     ]
 
 
+
 admin.site.register(Horario,HorarioAdmin)
+admin.site.register(Periodo,PeriodoAdmin)
 

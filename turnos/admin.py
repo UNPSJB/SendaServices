bdbe4e4a46b04aee1be6083206d5415764131f38
skarkloff <skarkloff.ls99@gmail.com>
2023-11-16 17:46:03 -0300

    turnos modelsv2

--- turnos/admin.py
+++ turnos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+
+
+
+class PeriodoInline(admin.TabularInline):
+    model = Periodo
+
+class HorarioAdmin(admin.ModelAdmin):
+    inlines = [
+        PeriodoInline
+    ]
+
+
+admin.site.register(Horario,HorarioAdmin)
+

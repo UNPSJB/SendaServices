d7bf221a7e75a04c3c50a600ca31d866e199c6bd
AlejooGz <ale.maxigonzalez@gmail.com>
2023-11-26 13:46:38 -0300

    resolviendo confilictos entre inmuebles y turnos

--- turnos/urls.py
+++ turnos/urls.py
@@ -4,9 +4,9 @@ from .views import HorarioCreateView,HorarioListView,HorarioUpdateView
 app_name= "turnos"
 
 urlpatterns = [
-    path('crear/',HorarioCreateView.as_view(),name="crearHorario"),
-    path('listar/',HorarioListView.as_view(), name='listarHorarios'),
-    path('modificar/<int:pk>/',HorarioUpdateView.as_view(), name='modificarHorario'),
+    path('horarios/crear/',HorarioCreateView.as_view(),name="crearHorario"),
+    path('horarios/listar/',HorarioListView.as_view(), name='listarHorarios'),
+    path('horarios/modificar/<int:pk>/',HorarioUpdateView.as_view(), name='modificarHorario'),
 
 
 

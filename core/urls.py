341982a8e5e5c5837bfa1b9067ba9484d7036a78
Gabriel Federico Jose Gimenez Miguel <gabylan02000@hotmail.com>
2024-08-10 20:06:26 -0300

    cambio en urls de empleado

--- core/urls.py
+++ core/urls.py
@@ -84,9 +84,10 @@ urlpatterns = [
   
     #Gestion Empleados
     path('empleado/', login_required(EmpleadoCreateView.as_view()), name="crearEmpleado"),
-    path('empleadoList/', EmpleadoListView.as_view(), name="listarEmpleado"),
-    path('empleadoModificar/<int:pk>', EmpleadoUpdateView.as_view(), name="modificarEmpleado"),
+    path('empleado/listar', EmpleadoListView.as_view(), name="listarEmpleado"),
+    path('empleado/modificar/<int:pk>', EmpleadoUpdateView.as_view(), name="modificarEmpleado"),
     path('empleado/eliminar/<int:pk>', EmpleadoDeleteView.as_view(), name='eliminarEmpleado'),
+    
     
     #Gestion Categoria
     path('categorias/',CategoriaCreateView.as_view(),name='crearCategoria'),

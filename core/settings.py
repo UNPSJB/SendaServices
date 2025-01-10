793aedbb96b8b0022526437773e896f67bba7821
unknown <Alejo Gonzalez>
2025-01-10 16:01:18 -0300

    commit para pull

--- core/settings.py
+++ core/settings.py
@@ -29,9 +29,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = os.getenv("DEBUG")
-
+#DEBUG = True
 
 ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['*']
 
 
 # Application definition

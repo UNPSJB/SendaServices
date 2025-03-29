e913aa0598afe42efe0fee2d556661dd9557416c
Lautaro Skarkloff <skarkloff.ls99@gmail.com>
2025-03-29 15:24:00 -0300

    conflicto en settings resuelto

--- core/settings.py
+++ core/settings.py
@@ -31,9 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG")
 #DEBUG = True
 
-ALLOWED_HOSTS = []
-#ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 # Application definition
 
